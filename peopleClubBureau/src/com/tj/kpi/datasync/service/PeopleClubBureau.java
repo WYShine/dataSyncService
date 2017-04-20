b9001ec0c35964ea19b9dcabb96a0733b7148bf5
qyc <originchenqy@gmail.com>
2017-04-20 15:02:14 +0800

    人社局初始化

---
+++ peopleClubBureau/src/com/tj/kpi/datasync/service/PeopleClubBureau.java
@@ -0,0 +1,7 @@
+package com.tj.kpi.datasync.service;
+
+/**
+ * Created by C310 on 2017/4/20.
+ */
+public interface PeopleClubBureau {
+}
---
+++ peopleClubBureau/src/com/tj/kpi/datasync/serviceimpl/PeopleClubBureauImpl.java
@@ -0,0 +1,10 @@
+package com.tj.kpi.datasync.serviceimpl;
+
+import com.tj.kpi.datasync.service.PeopleClubBureau;
+
+/**
+ * Created by C310 on 2017/4/20.
+ */
+public class PeopleClubBureauImpl implements PeopleClubBureau{
+
+}
